e3fcf889def1e17b45f03f0c44e729809060e2d2
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2022-04-28 20:28:16 +0700

    Update Source.cpp

--- RGR/Source.cpp
+++ RGR/Source.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <Windows.h>
 #define  CODE_ATBASH       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define  DECODE_ATBASH    "ZYXWVUTSRQPONMLKJIHGFEDCBA"
 
