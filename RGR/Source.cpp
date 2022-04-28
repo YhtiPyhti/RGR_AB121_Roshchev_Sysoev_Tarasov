91847b21905517931724e40c7809549572281350
1Nostradamus1 <mr.adsm@bk.ru>
2022-04-28 16:56:22 +0700

    +шифр

--- RGR/Source.cpp
+++ RGR/Source.cpp
@@ -102,3 +102,44 @@ void Tarab_G()
 
 
 }
+void atbash() {
+    char* code_atbash(char* src) {
+        char* dst = src;
+        const char* cch, * dch;
+
+        while (*src) {
+            if (isalpha(*src)) {
+                for (dch = DECODE_ATBASH, cch = CODE_ATBASH; *cch; *cch++, *dch++) {
+                    if (*cch == toupper(*src)) {
+                        *src = (isupper(*src)) ? *dch : tolower(*dch);
+                        break;
+                    }
+                }
+            }
+            *src++;
+        }
+        return dst;
+    }
+
+    // функция декодирования
+    char* decode_atbash(char* src) {
+        char* dst = src;
+        const char* cch, * dch;
+
+        while (*src) {
+            if (isalpha(*src)) {
+                for (dch = DECODE_ATBASH, cch = CODE_ATBASH; *cch; *cch++, *dch++) {
+                    if (*dch == toupper(*src)) {
+                        *src = (isupper(*src)) ? *cch : tolower(*cch);
+                        break;
+                    }
+                }
+            }
+            *src++;
+        }
+        return dst;
+    }
+
+
+   
+}
