7575fc0d61f55282c1ff598038d3653a53eb55f1
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2022-04-28 20:27:54 +0700

    Update RGR.cpp

--- RGR/RGR.cpp
+++ RGR/RGR.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <Windows.h>
 #define  CODE_ATBASH       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define  DECODE_ATBASH    "ZYXWVUTSRQPONMLKJIHGFEDCBA"
 
