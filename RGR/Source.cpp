7a3f8653e4cb104565093967d610a29efdcc6914
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2022-06-03 18:37:01 +0700

    Update Source.cpp

--- RGR/Source.cpp
+++ RGR/Source.cpp
@@ -132,6 +132,67 @@ void Tarab_G()
 
      }
   cout <<"Зашифрованное слово: "<< str << endl;
+	for (int i = 0; i < 1000; i++) 
+    {
+     if (str[i] == 'Б') 
+     {
+       str[i] = 'Щ';
+    }
+     else if (str[i] == 'Щ')
+        str[i] = 'Б';
+
+     if (str[i] == 'В')
+        str[i] = 'Ш';
+     else if (str[i] == 'Ш')
+        str[i] = 'В';
+
+     if (str[i] == 'Г')
+        str[i] = 'Ч';
+     else if (str[i] == 'Ч')
+        str[i] = 'Г';
+
+     if (str[i] == 'Д')
+       str[i] = 'Ц';
+     else if (str[i] == 'Ц')
+	     str[i] = 'Д';
+
+    if (str[i] == 'Ж')
+        str[i] = 'Х';
+     else if (str[i] == 'Х')
+        str[i] = 'Ж';
+
+     if (str[i] == 'З') 
+       {       
+	str[i] = 'Ф';
+     }
+     else 
+     {
+       if (str[i] == 'Ф')
+            str[i] = 'З';
+     }
+
+     if (str[i] == 'К')
+        str[i] = 'Т';
+     else if (str[i] == 'Т')
+        str[i] = 'К';
+
+     if (str[i] == 'Л')
+        str[i] = 'С';
+     else if (str[i] == 'С')
+        str[i] = 'Л';
+
+     if (str[i] == 'М')
+        str[i] = 'Р';
+     else if (str[i] == 'Р')
+        str[i] = 'М';
+
+     if (str[i] == 'Н')
+        str[i] = 'П';
+     else if (str[i] == 'П')
+        str[i] = 'Н';
+
+     }
+  cout <<"Дешифрованное слово: "<< str << endl;
 
 
 }
